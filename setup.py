f8be40a5186fbb92edb09556a0e7ef34d9f068e2
Rafael da Hora Fraga <f.rafaelhora@gmail.com>
2021-07-22 19:43:06 -0300

    Creating cookiecutter project for ML

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Overview of a complete dataset of spotify streams from 2010 to 2016',
+    author='rdhf',
+    license='MIT',
+)
